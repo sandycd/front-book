9605931f41a2ec144ccb38e64123ea0fd8f75cf2
sandycd <15603064117@163.com>
2020-05-08 00:06:08 +0800

    提交上传

--- src/components/EbookUpload/index.vue
+++ src/components/EbookUpload/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="singleImageUpload2 upload-container">
-    <el-upload>
+    <el-upload
+    :action="action"
+    :headers="headers"
+    :multiple="false"
+    :limit="1"
+    :before-upload="beforeUpload"
+    :on-success="onSuccess"
+    :on-error="onError"
+    :on-remove="onRemove"
+    :file-list="fileList"
+    :on-exceed="onExceed"
+    :disabled="disabled"
+    drag
+    show-file-list
+    class="image-uploader"
+    accept="application/epub+zip"
+    >
       <i class="el-icon-upload" />
       <div v-if="fileList.length === 0" class="el-upload__text">
         请将电子书拖入或
@@ -14,14 +30,87 @@
   </div>
 </template>
 <script>
+import {getToken} from '@/utils/auth'
 export default {
+  name:'EbookUpload',
+  props:{
+    fileList:{
+      type:Array,
+      default(){
+        return []
+      }
+    },
+    disabled:{
+      type:Boolean,
+      default:false
+    }
+  },
   data(){
     return {
-      fileList:[]
+      action:`${process.env.VUE_APP_BASE_API}/book/upload`
+    }
+  },
+  computed:{
+    headers(){
+      return {
+        Authorization:`Bearer ${getToken()}`
+      }
+    }
+  },
+  methods:{
+    onRemove(){
+      this.$message({
+        message:"电子书删除成功",
+        type:"success"
+      })
+      this.$emit('onRemove')
+    },
+    onError(err){
+      const msg = (err.message &&  JSON.parse(err.message)) ||'上传失败'
+      this.$message({
+        message:msg,
+        type:"error"
+      })
+      this.$emit('onError',err)
+    },
+    onSuccess(res,file){
+      console.log('success',res,file);
+      const {data,code,msg} = res;
+      if(code !==0){
+          this.$message({
+            message:(msg && `上传失败，失败原因：${msg}`) || '上传失败',
+            type:"error"
+          })
+          this.$emit('onError',data)
+      }else{
+          this.$message({
+            message:"电子书上传成功",
+            type:"success"
+          })
+          this.$emit('onSuccess',data)
+      }
+    },
+    beforeUpload(file){
+      this.$emit('beforeUpload',file)
+    },
+    onExceed(){
+        this.$message({
+          message: '每次只能上传一本电子书',
+          type: 'warning'
+        })
     }
   }
+
 }
 </script>
 <style lang="scss">
-  
+    .upload-container {
+    width: 100%;
+    height: 100%;
+    position: relative;
+
+    .image-uploader {
+      height: 100%;
+    }
+  }
 </style>
--- src/views/book/component/Detail.vue
+++ src/views/book/component/Detail.vue
@@ -7,14 +7,110 @@
       {{isEdit?'编辑电子书':'新增电子书'}}</el-button>
       </sticky>
       <div class="detail-container">
-      <el-row>
-        <warning />
-        <el-col :row="24">
-          <ebook-upload></ebook-upload>
-        </el-col>
-        <el-col :row="24">
-        </el-col>
-      </el-row>
+        <el-row>
+          <warning />
+          <el-col :row="24">
+            <ebook-upload
+              v-model="postForm.image_url"
+              :file-list = "fileList"
+              :disabled ="isEdit"
+              @onSuccess="onUploadSuccess"
+              @onRemove="onUploadRemove"
+            ></ebook-upload>
+          </el-col>
+          <el-col :row="24">
+            <el-form-item style="margin-bottom:24px" prop="title">
+              <MDinput v-model="postForm.title" :maxlength="100" name="name" required>书名</MDinput>
+            </el-form-item>
+            <div>
+              <el-row>
+                <el-col :span="12" class="form-item-author">
+                  <el-form-item :label-width="labelWidth" label="作者:">
+                    <el-input
+                      v-model="postForm.author"
+                      placeholder="作者"
+                      style="width:100%"
+                    ></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item :label-width="labelWidth" label="出版社:">
+                    <el-input
+                      v-model="postForm.publisher"
+                      placeholder="出版社"
+                      style="width:100%"
+                    ></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item :label-width="labelWidth" label="语言:">
+                    <el-input
+                      v-model="postForm.language"
+                      placeholder="语言"
+                      style="width:100%"
+                    ></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item :label-width="labelWidth" label="根文件:">
+                    <el-input
+                      v-model="postForm.rootFile"
+                      placeholder="根文件"
+                      style="width:100%"
+                    ></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item :label-width="labelWidth" label="文件路径:">
+                    <el-input
+                      v-model="postForm.filePath"
+                      placeholder="文件路径"
+                      style="width:100%"
+                    ></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item :label-width="labelWidth" label="解压路径:">
+                    <el-input
+                      v-model="postForm.unzipPath"
+                      placeholder="解压路径"
+                      style="width:100%"
+                    ></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item :label-width="labelWidth" label="封面路径:">
+                    <el-input
+                      v-model="postForm.coverPath"
+                      placeholder="封面路径"
+                      style="width:100%"
+                    ></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item :label-width="labelWidth" label="文件名称:">
+                    <el-input
+                      v-model="postForm.fileName"
+                      placeholder="文件名称"
+                      style="width:100%"
+                    ></el-input>
+                  </el-form-item>
+                </el-col>
+
+              </el-row>
+              <el-row>
+              <el-col :span="24">
+              <el-form-item label-width="60px" label="封面">
+                <a v-if="postForm.cover" :href="postForm.href" target="_blank">
+                  <img :src="postForm.cover" class="preview-img">
+                </a>
+                <span v-else>无</span>
+              </el-form-item>
+              </el-col>
+              </el-row>
+            </div>
+          </el-col>
+        </el-row>
       </div>
     </el-form>
   </div>
@@ -25,33 +121,34 @@ import Warning from './warning'
 import EbookUpload from '../../../components/EbookUpload/index'
 export default {
   name: 'Detail',
-  props:{
-    isEdit:Boolean
-  },
-  data(){
-    return {
-      loading:false,
-      postForm:{
-        status:'deleted'
-      }
-    }
-  },
-  components:{
+  components: {
     Sticky,
     Warning,
     EbookUpload
   },
-  methods:{
-    submitForm(){
+  props: {
+    isEdit: Boolean
+  },
+  data() {
+    return {
+      loading: false,
+      postForm: {
+        status: 'deleted'
+      }
+    }
+  },
+
+  methods: {
+    submitForm() {
       this.loading = true
       setTimeout(() => {
         this.loading = false
-      },1000)
+      }, 1000)
     }
   }
 }
 </script>
-<style lang="scss" scope>  
+<style lang="scss" scope>
   @import "~@/styles/mixin.scss";
   .detail{
     position: relative;
